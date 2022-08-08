b319b58634eac226e403069001b1696f2afbb6e5
sebajgastia <sebastian.gastia@gmail.com>
2022-08-08 20:01:31 -0300

    se agrego una imagen para el html y se realizaron trabajos a js

--- js/style.js
+++ js/style.js
@@ -1,40 +1,83 @@
-//precios y cantidades
+//bienvenida
 
-let Cant = parseInt(prompt("cuantos potes de helado vas a llevar?"));
+alert("Hola! que bueno tenerte por aca! presionar enter");
+let Nombre = prompt("ingresa tu nombre!");
+let Ingresecel = parseInt(prompt("ingresa tu numero de celular!"));
 
-let PreciosHelados = 0;
-let PrecioTotal = 0; 
-
-//calcular precio helados
-
-for ( let j = 0; j < Cant; j++) {
-    let preciosinIva = parseFloat(prompt("ingrese el precio del helado elegido"));
-    let PreciomasIva = CalculaIva(preciosinIva);
-    PreciosHelados += PreciomasIva;
-    break;
+if ((Ingresecel != "") && (Nombre !="")){
     
-}
+    alert( "bienvenido: " + "Tu nombre es: " + Nombre+ "Tu celular es: " +Ingresecel);
 
-//descuento de helados 
-
-if (Cant <= 3 ) {
-    PrecioTotal = PreciosHelados;
-} else if (Cant >= 3 && Cant <= 10) {
-    let Descontar = (PreciosHelados * 10) / 100;
-    PrecioTotal = PreciosHelados - Descontar;
-}
-else if(Cant >= 10) {
-    let Descontar = (PreciosHelados * 20) /100;
-    PrecioTotal = PreciosHelados - Descontar;    
 }else{
-    alert("Fatal: ingresa el precio!");
+    alert("FATAL: por favor ingrese su nombre y numero celular");
 }
 
-//funcion
+//productos
 
-function CalculaIva(totalimpuesto){
-    let PreciomasIva = totalimpuesto * 2.20;
-    return PreciomasIva;
+const productos = [
+    { nombre: "helado", precio: 800},
+    { nombre: "paletas", precio: 400},
+    { nombre: "tortas heladas", precio: 1500},
+    { nombre: "trufas heladas", precio: 1000},
+];
+
+//agregar al chango
+
+let changuitoDecompras = []
+
+let seleccionarProducto = prompt("quiere comprar un producto? si/no")
+
+while(seleccionarProducto != "si" && seleccionarProducto != "no"){
+    alert("ingrese si o no")
+    //seleccionarProducto = prompt("Desea comprar algun producto? si o no")
+    
+};
+
+if(seleccionarProducto == "si"){
+    alert("Nuestra lista de productos!")
+    let Losproductos = productos.map((producto) => producto.nombre + "" + producto.precio + "$");
+    alert(Losproductos.join (" _ "))
+} else if (seleccionarProducto == "no"){
+    alert("Que tengas un bonito dia!")
 }
-console.log("total: " + PreciosHelados);
-console.log("total con su respectivo descuento: " + PrecioTotal);
+
+while(seleccionarProducto != "no"){
+    let productos = prompt("agregar producto a tu carrito")
+    let precio = 0
+
+    if(productos == "helado" || productos == "paletas" || productos == "tortas heladas" || productos == "trufas heladas" ){
+      switch(productos){
+        case "helado":
+         precio = 800;
+         break;
+        case "paletas":
+         precio = 400;
+         break;
+        case "tortas heladas":
+         precio = 1500;
+         break;
+        case "trufas heladas":
+         precio = 1200;
+         break;
+        
+        }
+    let unidades = parseInt(prompt("cuantas unidades quiere llevar?"))
+    
+    changuitoDecompras.push({productos, unidades, precio})
+    }else{
+        alert("FATAL: no tenemos ese producto")
+    }
+    seleccionarProducto = prompt("Desea continuar con la compra? si/no")
+
+    while(seleccionarProducto === "no"){
+        alert("gracias por tu compra! vuelve pronto!")
+        changuitoDecompras.forEach((changofinal) => {
+            console.log(`producto: ${changofinal.productos}, unidades: ${changofinal.unidades} su total ${changofinal.unidades * changofinal.precio}`)
+        })
+        break;
+    }
+}
+function total(){
+   let precioTotal = changuitoDecompras.reduce(((acc, el) => acc + el.precio * el.unidades, 0))
+    return precioTotal(`el total a pagar es: ${total}`);
+}
